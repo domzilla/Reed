e9a361bef69ff12ec0cdc8496dc91d054a1f6a28
Dominic Rodemer <dominic.rodemer@me.com>
2025-12-30 08:31:08 +0100

    Remove buildscripts directory

--- buildscripts/updateSecrets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find "${PROJECT_DIR}" -name '*.gyb' |
-  while read file; do
-    echo "Generating ${file%.gyb}";
-    "${PROJECT_DIR}/buildscripts/gyb" --line-directive '' -o "${file%.gyb}" "$file";
-  done
