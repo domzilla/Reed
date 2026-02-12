54b07ebb5ea3c5b7d16272a4683a07f69a3889c6
Dominic Rodemer <dominic.rodemer@me.com>
2026-02-12 16:58:16 +0100

    Rename stale QSDatabase category to RDDatabase in NSString+RDDatabase.h

--- src/Reed/Classes/Database/ObjC/NSString+RDDatabase.h
+++ src/Reed/Classes/Database/ObjC/NSString+RDDatabase.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface NSString (QSDatabase)
+@interface NSString (RDDatabase)
 
 
 /*Returns @"(?, ?, ?)" -- where number of ? spots is specified by numberOfValues.
